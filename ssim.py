910dc159660b9e9a7c12eabf60252818d11cc07c
Joshua Reibert <joshua.reibert@gmail.com>
2017-08-11 18:52:39 +0200

    Add main python file that loads images given as arguments

---
+++ ssim.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+import argparse
+from PIL import Image
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Compute the Structural Similarity Index of two images.')
+    parser.add_argument('image')
+    parser.add_argument('ref_image')
+    args = parser.parse_args()
+
+    image = Image.open(args.image)
+    ref_image = Image.open(args.ref_image)
+
+    if not image.size == ref_image.size:
+        print('Error: The two images need to have the same size!')
+        return
+
+
+if __name__ == '__main__':
+    main()
