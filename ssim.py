fd77c71044431422e3ee45de86b5c94f283c9400
Joshua Reibert <joshua.reibert@gmail.com>
2017-08-12 12:50:35 +0200

    Add dummy ssim function and print its result

--- ssim.py
+++ ssim.py
@@ -4,6 +4,10 @@ import argparse
 from PIL import Image
 
 
+def ssim(image, ref_image):
+    return 0.0
+
+
 def main():
     parser = argparse.ArgumentParser(
         description='Compute the Structural Similarity Index of two images.')
@@ -17,6 +21,8 @@ def main():
     if not image.size == ref_image.size:
         print('Error: The two images need to have the same size!')
         return
+
+    print(ssim(image, ref_image))
 
 
 if __name__ == '__main__':
